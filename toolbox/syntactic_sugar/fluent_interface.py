ca117d352dc62040ac16c414a45a22ac913d0d66
Xiang Zhang <xzhang1@mit.edu>
2020-03-27 17:27:56 -0400

    .

--- toolbox/syntactic_sugar/fluent_interface.py
+++ toolbox/syntactic_sugar/fluent_interface.py
@@ -1,9 +1,24 @@
-from toolbox.barebones.functions import d_struct_to_vasp
+from toolbox.barebones.functions import d_struct_to_vasp, to_slurm, submit, is_complete, retrieve
 from toolbox.barebones.objects import Struct as BarebonesStruct
 
 class Struct(BarebonesStruct):
-    # Struct().to_vasp(d).to_slurm(d).submit(d).retrieve(d)
+    # Struct().to_vasp(d).to_slurm(d).submit().retrieve(d)
 
     def to_vasp(self, d):
         d_struct_to_vasp(d, self)
+        return self
+
+    def to_slurm(self, d):
+        to_slurm(d)
+        return self
+
+    def submit(self):
+        submit()
+        return self
+
+    def is_complete(self, d):
+        return is_complete(d)
+
+    def retrieve(self, d):
+        retrieve(d)
         return self
