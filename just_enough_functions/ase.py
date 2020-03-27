7d9efc92a1b8ea14f447320c00c204e79261f66a
Xiang Zhang <xzhang1@mit.edu>
2020-03-26 23:53:40 -0400

    no cute __call__. explicit is better than implicit. less code is better than more.

--- just_enough_functions/ase.py
+++ just_enough_functions/ase.py
@@ -14,9 +14,11 @@ def _struct_to_atoms(struct):
     return ase.Atoms(symbols=symbols, cell=cell, positions=positions)
 
 def read(*args, **kwargs):
+    """ase.io.read but to struct"""
     atoms = ase.io.read(*args, **kwargs)
     return _atoms_to_struct(atoms)
 
 def write(filename, struct, *args, **kwargs):
+    """ase.io.write but from struct"""
     atoms = _struct_to_atoms(struct)
     return ase.io.write(filename, atoms, *args, **kwargs)
--- tf.py
+++ tf.py
@@ -1,24 +1,48 @@
 class Op:
+    """
+    Example:
+        >>> import operator
+        >>> add = Op(operator.add)
+        >>> add(1, 2).eval()
+        3
+
+    Attributes:
+        function (PrimitiveType -> PrimitiveType [1]_):
+        inputs (list of Tensor or PrimitiveType):
+        output (Tensor);
+
+    .. [1] Not necessarily primitiveType, but "regular" (i.e. non-Tensor) types.
+    """
 
     def __init__(self, function):
+        # Sets function
         self.function = function
 
     def __call__(self, *inputs):
+        """Sets inputs. Returns output Tensor.
+
+        Just as in Tensorflow, this does 2 things
+        """
         self.inputs = inputs
-        self.output = Tensor(self)
+        self.output = Tensor(op=self)
         return self.output
 
     def run(self):
+        # Returns: PrimitiveType
         return self.function(*(i.eval() if i is Tensor else i for i in self.inputs))
 
 class Tensor:
-
+    """
+    Attributes:
+        op (Op): The Op whose output is this Tensor.
+        value (PrimitiveType): Cached value.
+    """
     def __init__(self, op, value=None):
         self.op = op
         self.value = value
 
     def eval(self):
+        # Returns: PrimitiveType
         if self.value is None:
             self.value = self.op.run()
         return self.value
-
