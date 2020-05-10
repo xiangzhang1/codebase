de608b3bffd56122c1f01de1e866c853308ea27e
Xiang Zhang <xzhang1@mit.edu>
2020-05-10 03:32:21 -0400

    bugfix

--- codebase/toolkit/optional/functions/struct.py
+++ codebase/toolkit/optional/functions/struct.py
@@ -3,8 +3,9 @@ import pandas as pd
 from codebase.toolkit.objects import Struct
 
 def axs_to_struct(A, X, S):
+    """No such thing as pd.DataFrame(data1, columns1, data2, columns2) """
     XS = pd.concat([
         pd.DataFrame(X, columns=('X', 'Y', 'Z')),
         pd.Series(S, name='S')
-    ])
+    ], axis=1)
     return Struct(A=A, XS=XS)
