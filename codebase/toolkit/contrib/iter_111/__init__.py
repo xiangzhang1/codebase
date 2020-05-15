7e5a8e03fc1c6e7ce44348e056b1c3aa2e931b5c
Xiang Zhang <xzhang1@mit.edu>
2020-05-15 01:37:58 -0400

    Oops, deleted slurm submit script.

--- codebase/toolkit/contrib/iter_111/__init__.py
+++ codebase/toolkit/contrib/iter_111/__init__.py
@@ -6,17 +6,17 @@ import pandas as pd
 from codebase.toolkit.common import dict2str, template
 from codebase.toolkit.io.vasp import struct2poscar
 
+ASSETS = os.path.join(os.path.dirname(__file__), 'assets')
+
+
 sample_d = {
     'cluster': 'knl',
     'queue': 'regular',
     'nnode': 4
 }
 
-ASSETS = os.path.join(os.path.dirname(__file__), 'assets')
 
-
-def prepare(d, struct):
-    d['job_name'] = dict2str(struct.stoichiometry) + '_' + ''.join(random.choices(string.ascii_letters + string.digits, k=4))
+def exec_(d):
     d.exec_file(f"{ASSETS}/rules.py")
 
 
@@ -34,24 +34,34 @@ def vasp(d, struct):
 
 def job(d):
     PREFIX = os.path.join(ASSETS, 'job')
-    template(i=f"{PREFIX}/job/{d['cluster']}", o="job", d=d)
+    template(i=f"{PREFIX}/{d['cluster']}", o="job", d=d)
 
 
-def default_submit(d):
-    host = 'cori' if d['cluster'] in ['knl', 'haswell'] else d['cluster']
-    hosttype = {
-        ''
+def auto_submit(d, struct):
+    # hostname, local, remote, job_name
+    if d['cluster'] in ['knl', 'haswell']:
+        hostname = 'cori'
+    else:
+        hostname = d['cluster']
+    local = os.getcwd()
+    uid = dict2str(struct.stoichiometry) + '_' + ''.join(random.choices(string.ascii_letters + string.digits, k=4))
+    if hostname == 'comet':
+        remote = f"/oasis/scratch/comet/xzhang1/temp_project/{uuid}"
+    elif hostname == 'cori':
+        remote = f"/global/cscratch1/sd/xzhang1/{uuid}"
+    else:
+        remote = uid
+    job_name = uid
+    # jobdict
+    jobdict = {
+        'hostname': hostname,
+        'local': local,
+        'remote': remote,
+        'job_name': job_name
     }
-    subprocess.run(f"rsync", shell=True)
-
-
-
-def for_listener(d, listener):
-    listener.jobs.append(
-        pd.Series({
-            'cluster':
-            'local': os.getcwd()
-        }, name=d['job_name'])
-    )
+    # submit
+    PREFIX = os.path.join(ASSETS, 'submit')
+    if hostname in ['comet', 'cori', 'eccle', 'irmik', 'nanaimo']:
+        template(i=f"{PREFIX}/slurm", o="submit", d=jobdict)
 
 
