1d7310d7f87f07b3adfae7cb448aa28e1644fe7a
Xiang Zhang <xzhang1@mit.edu>
2020-05-14 23:06:33 -0400

    Let's remove 'submit'!

--- codebase/toolkit/slurm/listener.py
+++ codebase/toolkit/slurm/listener.py
@@ -1,23 +1,43 @@
 import os
+import subprocess
 import pandas as pd
 
 ASSETS = os.path.join(os.path.dirname(__file__), 'assets')
 
-class SlurmListener:
+
+class Listener:
     """
     Attributes
     ----------
-    jobs : pandas.DataFrame(columns=(cluster, job_name, local, remote, status))
-        job_name is assumed to be unique.
+    jobs : pandas.DataFrame::
+
+                 cluster           local              remote state
+        job_name
+        Pb140S85    cori  /home/xzhang1/  /global/cscratch1/   NaN
+        dellpc       NaN             NaN                 NaN   NaN
     """
 
     def __init__(self):
-        self.jobs = pd.DataFrame(columns=['cluster', 'job_name', 'local', 'remote', 'status'])
+        self.jobs = pd.DataFrame(columns=['job_name', 'cluster', 'local', 'remote', 'state']).set_index('job_name')
 
-    def register(self, **job):
-        assert set(job.keys()) <= set(self.jobs.columns)
-        self.jobs = self.jobs.append(job)
+    def register(self, job_name, **details):
+        self.jobs.loc[job_name] = details
 
-    def refresh(self):
-        for cluster in self.jobs.cluster:
+    def squeue(self):
+        self.jobs.state = 'NOT_IN_SQUEUE'
+        for cluster in self.jobs.cluster.unique():
+            subprocess.run(f"bash {ASSETS}/squeue.{cluster}") # generates squeue.output, columns=(NAME, STATE)
+            self.jobs.update(
+                pd.read_csv('refresh.output', delim_whitespace=True)
+                  .rename({'NAME': 'job_name', 'STATE': 'state'})
+                  .set_index('job_name')
+            )
+
+    def retrieve(self):
+        for index, row in self.jobs.iterrows():
+            if row.state == 'PRESUMED_COMPLETE' and row.remote:
+                subprocess.run(
+                    f"rsync -a --info=progress2 {row.cluster}:{row.remote} {row.local}"
+                ) # careful trailing slash
+
 
