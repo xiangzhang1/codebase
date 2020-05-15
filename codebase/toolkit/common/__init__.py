71063be474b131288b35787b67092f35dd926486
Xiang Zhang <xzhang1@mit.edu>
2020-05-15 00:03:40 -0400

    What's submit again?

--- codebase/toolkit/common/__init__.py
+++ codebase/toolkit/common/__init__.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 import pandas as pd
 import dill
 
--- codebase/toolkit/contrib/iter_111/__init__.py
+++ codebase/toolkit/contrib/iter_111/__init__.py
@@ -1,5 +1,7 @@
 import os
 from shutil import copy
+import random
+import string
 import pandas as pd
 from codebase.toolkit.common import dict2str, template
 from codebase.toolkit.io.vasp import struct2poscar
@@ -13,8 +15,9 @@ sample_d = {
 ASSETS = os.path.join(os.path.dirname(__file__), 'assets')
 
 
-def exec_(d):
-    d.exec_file(f"{ASSETS}/rules.py")
+def prepare(d, struct):
+    d['job_name'] = dict2str(struct.stoichiometry) + '_' + ''.join(random.choices(string.ascii_letters + string.digits, k=4))
+    d.exec_file(f"{ASSETS}/d.py")
 
 
 def to_vasp(d, struct):
@@ -35,13 +38,15 @@ def to_slurm(d):
 
 
 def submit(d):
+    cluster = 'cori' if d['cluster'] in ['knl', 'haswell'] else d['cluster'],
+    subprocess.run(f"rsync", shell=True)
 
 
 
 def for_listener(d, listener):
     listener.jobs.append(
         pd.Series({
-            'cluster': 'cori' if d['cluster'] in ['knl', 'haswell'] else d['cluster'],
+            'cluster':
             'local': os.getcwd()
         }, name=d['job_name'])
     )
