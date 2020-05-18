a670211e93395fbe32d9853878c1b2c995e302e7
Xiang Zhang <xzhang1@mit.edu>
2020-05-18 13:20:55 -0400

    i think i mistakenly checked out some rando files

--- apps/manager/__init__.py
+++ apps/manager/__init__.py
@@ -1,12 +1,12 @@
 import os
 from apps.manager.json import load, dump
 
-PROJECTS = os.path.dirname(__file__)
+SAVE_FOLDER = os.path.dirname(__file__)
 
 
 def load_manager():
-    return load(f"{PROJECTS}/manager.json")
+    return load(f"{SAVE_FOLDER}/manager.json")
 
 
 def dump_manager(manager):
-    dump(manager, f"{PROJECTS}/manager.json")
+    dump(manager, f"{SAVE_FOLDER}/manager.json")
