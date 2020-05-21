622f810a1f4ef56d7f96c71d991ddf05e154045b
Xiang Zhang <xzhang1@mit.edu>
2020-05-20 22:51:47 -0400

    added toolkit.functions.pad

--- toolkit/functions.py
+++ toolkit/functions.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from toolkit.struct import Struct
 
 
 def XS(X, S):
@@ -12,3 +14,13 @@ def exec_file(fname, d):
         exec(f.read(), globals(), d)
 
 
+def pad(struct, pad):
+    X = struct.XS[['X', 'Y', 'Z']]
+    S = struct.XS.S
+
+    X -= np.amin(X, axis=0)
+    box = np.amax(X, axis=0)
+
+    A = np.diag(box + 2 * pad)
+    X += pad
+    return Struct(A=A, XS=XS(X, S))
