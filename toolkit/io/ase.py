462be5a5c8fd6d794e3f62946ce4461c82f378b9
Xiang Zhang <xzhang1@mit.edu>
2020-05-23 15:43:01 -0400

    removed toolkit.io.ase.read/write, kept struct2atoms and atoms2struct

--- toolkit/io/ase.py
+++ toolkit/io/ase.py
@@ -7,7 +7,7 @@ from toolkit.functions import XS
 """ASE writes POSCAR4."""
 
 
-def atoms_to_struct(atoms):
+def atoms2struct(atoms):
     return Struct(
         A=atoms.get_cell(),
         XS=XS(
@@ -17,25 +17,9 @@ def atoms_to_struct(atoms):
     )
 
 
-def struct_to_atoms(struct):
+def struct2atoms(struct):
     return ase.Atoms(
         symbols=toolkit.functions.XS['S'].values,
         cell=struct.A,
         positions=toolkit.functions.XS[['X', 'Y', 'Z']].values
     )
-
-
-def read(*args, **kwargs):
-    """ase.io.read but to struct"""
-    atoms = ase.io.read(*args, **kwargs)
-    return _atoms_to_struct(atoms)
-
-
-def write(fname, struct, *args, **kwargs):
-    """
-    ase.io.write but from struct
-
-    Writes POSCAR4.
-    """
-    atoms = _struct_to_atoms(struct)
-    return ase.io.write(fname, atoms, *args, **kwargs)
